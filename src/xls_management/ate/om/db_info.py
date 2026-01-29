1db8fde3004482d020f56821cacc2f53c3d5266f
Jose Ayude Vazquez <jose.ayude@alten.es>
2026-01-29 16:48:35 +0100

    EinlesenDatei_Projectspezifisch -> db_info.py

--- src/xls_management/ate/om/db_info.py
+++ src/xls_management/ate/om/db_info.py
@@ -47,3 +47,153 @@ class DBInfo:
                         f"{', '.join(missing_attributes)}\n"
                     )
         return False, error_trace
+    
+#   Public Function EinlesenDatei_Projektspezifisch(ByVal strTitel As String, ByRef strAttribute() As String, ByRef rngAttribute() As Range, ByRef wbImport As Workbook, ByRef wksImport As Worksheet, ByRef strFehler As String, ByRef strDateinamen As String, _
+#                                                       ByVal strProjekt As String, ByRef strAttributeProjekt() As String, ByRef rngAttributeProjekt() As Range) As Boolean
+#       Dim blnAttributeImport As Boolean   'Flag für Attribute
+#       Dim blnAttributeProjektImport As Boolean    'Flag für projektspezifische Attribute
+#       Dim intWksImport As Integer         'Zähler für Worksheets
+#       Dim strImportPfad As String         'String für Dateipfad
+#       Dim strImportDatei As String        'String für Dateinamen
+#       Dim strFehlerGesamt As String       'Sting für Gesamtfehler
+#       Dim intAttributeZaehler As Integer  'Zähler für Attribute
+#       Dim strFehlerAttribute As String    'String für fehlende Attribute
+#       Dim intAttributeProjektZaehler As Integer   'Zähler für Projekt-Attribute
+#       Dim strFehlerProjektAttribute As String 'String für fehlende Projekt-Attribute
+#       Dim blnErsterFund As Boolean        'Flag für erstes Auffinden des vollständigen Attributesatzes
+#       Dim rngAttributeErsterFund() As Range   'Range-Array für erstes Auffinden des vollständigen allgemeinen Attributesatzes
+#       Dim rngAttributeProjektErsterFund() As Range    'Range-Array für erstes Auffinden des vollständigen projektspezifischen Attributesatzes
+#       Dim wksImportErsterFund As Worksheet    'Worksheet für erstes Auffinden des vollständigen Attributesatzes
+#       Dim lngZeilenZahlerErsterFund As Long   'Long für letzte Zeile des ersten Fundes
+#       Dim lngWeitererFundZaehler As Long  'Long für Startzeile des zusätzlichen Datensatzen
+#       
+#       On Error Resume Next
+#       
+#       blnAttributeImport = False
+#       blnAttributeProjektImport = False
+#       strFehlerGesamt = ""
+#       strDateinamen = ""
+#       blnErsterFund = False
+#       
+#       strImportPfad = Application.GetOpenFilename(FileFilter:="Excel-Dateien (*.xls; *.xlsx; *.xlm; *.xlsm), *.xls; *.xlsx; *.xlm; *.xlsm", FilterIndex:=1, Title:=strTitel & " auswählen")
+#       If Trim(strImportPfad) <> "Falsch" Then
+#           'Datei öffnen
+#           Workbooks.Open Filename:=strImportPfad, ReadOnly:=True
+#           'Dateinamen extrahieren
+#           strImportDatei = Right(strImportPfad, Len(strImportPfad) - InStrRev(strImportPfad, "\"))
+#           strDateinamen = strImportDatei
+#           'Workbook zuweisen
+#           Set wbImport = Workbooks(strImportDatei)
+#           'Worksheets nach Attributen durchsuchen
+#           intWksImport = 0
+#           Do
+#               'Rangeobjekte zurücksetzen
+#               ReDim rngAttribute(LBound(strAttribute, 1) To UBound(strAttribute, 1))
+#               ReDim rngAttributeProjekt(LBound(strAttributeProjekt, 1) To UBound(strAttributeProjekt, 1))
+#               
+#               'Zähler für Arbeitsblatt erhöhen
+#               intWksImport = intWksImport + 1
+#               'Worksheet zuweisen
+#               Set wksImport = wbImport.Sheets(intWksImport)
+#               
+#               'Allgemeine Attribute suchen
+#               For intAttributeZaehler = LBound(strAttribute, 1) To UBound(strAttribute, 1)
+#                   Set rngAttribute(intAttributeZaehler) = wksImport.Cells.Find(strAttribute(intAttributeZaehler), lookat:=xlWhole)
+#               Next intAttributeZaehler
+#               
+#               'Projekt-Attribute suchen
+#               For intAttributeProjektZaehler = LBound(strAttributeProjekt, 1) To UBound(strAttributeProjekt, 1)
+#                   Set rngAttributeProjekt(intAttributeProjektZaehler) = wksImport.Cells.Find(strAttributeProjekt(intAttributeProjektZaehler), lookat:=xlWhole)
+#               Next intAttributeProjektZaehler
+#               
+#               'Suche der allgemeinen Attribute auswerten
+#               strFehlerAttribute = ""
+#               If RangeObjekteVorhandenFehlerausgabe(rngAttribute, strAttribute, strFehlerAttribute) Then
+#                   blnAttributeImport = True
+#               Else
+#                   If strFehlerGesamt = "" Then
+#                       strFehlerGesamt = wksImport.Name & ": " & strFehlerAttribute
+#                   Else
+#                       strFehlerGesamt = strFehlerGesamt & "; " & wksImport.Name & ": " & strFehlerAttribute
+#                   End If
+#               End If
+#               
+#               'Suche der Projekt-Attribute auswerten
+#               strFehlerProjektAttribute = ""
+#               If RangeObjekteVorhandenFehlerausgabe(rngAttributeProjekt, strAttributeProjekt, strFehlerProjektAttribute) Then
+#                   blnAttributeProjektImport = True
+#               Else
+#                   If strFehlerGesamt = "" Then
+#                       strFehlerGesamt = wksImport.Name & ": " & strProjekt & " - " & strFehlerProjektAttribute
+#                   Else
+#                       strFehlerGesamt = strFehlerGesamt & "; " & wksImport.Name & ": " & strProjekt & " - " & strFehlerProjektAttribute
+#                   End If
+#               End If
+#               
+#               'Auswertung beider Suchen (allgemein und projektspezifisch) und Unterscheidung, ob es das erste Auffinden des vollständigen Attributsatzes ist oder weitere
+#               'Beim ersten Auffinden werden die beiden Range-Arays gespeichert
+#               'Ab dem zweitem Auffinden des vollständigen Attributsatzes erfolgt eine Abfrage, eine Kopiervorganges der dazugehörigen Daten zum ersten Datensatz erfolgen soll und ggf. Umsetzung
+#               If blnAttributeImport = True And blnAttributeProjektImport = True Then
+#                   'Zurücksetzen der Flags für den einzelnen Durchlauf
+#                   blnAttributeImport = False
+#                   blnAttributeProjektImport = False
+#                   
+#                   If blnErsterFund = False Then
+#                       blnErsterFund = True
+#                       'Felder dimensionieren
+#                       ReDim rngAttributeErsterFund(LBound(strAttribute, 1) To UBound(strAttribute, 1))
+#                       ReDim rngAttributeProjektErsterFund(LBound(strAttributeProjekt, 1) To UBound(strAttributeProjekt, 1))
+#                       'Übernahme allgemeine Attribute
+#                       For intAttributeZaehler = LBound(strAttribute, 1) To UBound(strAttribute, 1)
+#                           Set rngAttributeErsterFund(intAttributeZaehler) = rngAttribute(intAttributeZaehler)
+#                       Next intAttributeZaehler
+#                       'Übernahme projektspezifische Attribute
+#                       For intAttributeProjektZaehler = LBound(strAttributeProjekt, 1) To UBound(strAttributeProjekt, 1)
+#                           Set rngAttributeProjektErsterFund(intAttributeProjektZaehler) = rngAttributeProjekt(intAttributeProjektZaehler)
+#                       Next intAttributeProjektZaehler
+#                       'Übernahme Arbeitsblatt
+#                       Set wksImportErsterFund = wksImport
+#                   Else
+#                       If MsgBox("Weiterer Datensatz gefunden!" & vbCrLf & "Erster Fund: " & wksImportErsterFund.Name & vbCrLf & "Aktueller Fund: " & wksImport.Name & vbCrLf & vbCrLf & "Sollen die Datensätze zusammengeführt werden?", vbYesNo) = vbYes Then
+#                           'Alle Zeilen mit Einträgen durchlaufen und kopieren
+#                           For lngWeitererFundZaehler = 1 To wksImport.UsedRange.Rows.Count - rngAttribute(1).Row
+#                               If rngAttribute(1).Offset(lngWeitererFundZaehler, 0).Value <> "" Then
+#                                   'Zielzeile ermitteln
+#                                   lngZeilenZahlerErsterFund = rngAttributeErsterFund(1).End(xlDown).Row + 1
+#                                   
+#                                   'Allgemeine Werte kopieren
+#                                   For intAttributeZaehler = LBound(strAttribute, 1) To UBound(strAttribute, 1)
+#                                       wksImportErsterFund.Cells(lngZeilenZahlerErsterFund, rngAttributeErsterFund(intAttributeZaehler).Column).Value = rngAttribute(intAttributeZaehler).Offset(lngWeitererFundZaehler).Value
+#                                   Next intAttributeZaehler
+#                                   
+#                                   'Projektspezifische Werte kopieren
+#                                   For intAttributeProjektZaehler = LBound(strAttributeProjekt, 1) To UBound(strAttributeProjekt, 1)
+#                                       wksImportErsterFund.Cells(lngZeilenZahlerErsterFund, rngAttributeProjektErsterFund(intAttributeProjektZaehler).Column).Value = rngAttributeProjekt(intAttributeProjektZaehler).Offset(lngWeitererFundZaehler).Value
+#                                   Next intAttributeProjektZaehler
+#                               End If
+#                           Next lngWeitererFundZaehler
+#                       End If
+#                   End If
+#               End If
+#               
+#           'Loop While intWksImport < wbImport.Worksheets.Count And blnAttributeImport = False And blnAttributeProjektImport = False
+#           Loop While intWksImport < wbImport.Worksheets.Count
+#       End If
+#       
+#       
+#       'Rückgabe allgemeine Attribute
+#       For intAttributeZaehler = LBound(strAttribute, 1) To UBound(strAttribute, 1)
+#           Set rngAttribute(intAttributeZaehler) = rngAttributeErsterFund(intAttributeZaehler)
+#       Next intAttributeZaehler
+#       'Rückgabe projektspezifische Attribute
+#       For intAttributeProjektZaehler = LBound(strAttributeProjekt, 1) To UBound(strAttributeProjekt, 1)
+#           Set rngAttributeProjekt(intAttributeProjektZaehler) = rngAttributeProjektErsterFund(intAttributeProjektZaehler)
+#       Next intAttributeProjektZaehler
+#       'Rückgabe Arbeitsblatt
+#       Set wksImport = wksImportErsterFund
+#       
+#       'Rückgabewert übernehmen
+#       EinlesenDatei_Projektspezifisch = blnErsterFund
+#       'Fehlerwert übernehmen
+#       strFehler = strFehlerGesamt
+#   End Function
--- src/xls_management/ate/tracking.py
+++ src/xls_management/ate/tracking.py
@@ -1,4 +1,5 @@
-from xls_management.tui.file_picker import path_from_file_picker
+from xls_management.ate.om.db_info import DBInfo 
+from xls_management.ate.om.verificationskriterium  import Verificationskriterium
 
 #Option Explicit
 #
@@ -316,7 +317,7 @@ class ATEStatus():
 #       ElseIf EinlesenDatei("Anforderungen Projektbereich", strAVWAttribute, rngAVWAttribute, wbAVW, wksAVW, strFehlerAVW, strDateinamen(1)) Then
 #           ImportAttribute(1) = True
         else:
-            self.import_attribute[0] = self.einlessen_datei()
+            self.import_attribute[0] = self.einlesen_datei("Anforderungen Projektbereich")
         if not self.import_attribute[0]:
 #           'Sammlung aller gesuchten Attribute erzeugen
 #           strAttributeAVW = ""
@@ -359,7 +360,7 @@ class ATEStatus():
 #           'Dateiauswahl und Zuordnung
 #           If EinlesenDatei("Verifikationskriterien", strTDVKAttribute, rngTDVKAttribute, wbTDVK, wksTDVK, strFehlerTDVK, strDateinamen(2)) Then
 #               blnImportAttribute(2) = True
-            self.import_attribute[1] = self.info_TDVK.einlesen_datei()
+            self.import_attribute[1] = self.info_TDVK.einlesen_datei("Verifikationskriterien")
 #           Else
 #               'Sammlung aller gesuchten Attribute erzeugen
 #               strAttributeTDVK = ""
@@ -405,7 +406,7 @@ class ATEStatus():
 #           'Dateiauswahl und Zuordnung
 #           If EinlesenDatei("Absicherungsaufträge", strTDAAAttribute, rngTDAAAttribute, wbTDAA, wksTDAA, strFehlerTDAA, strDateinamen(3)) Then
 #               blnImportAttribute(3) = True
-            self.import_attribute[3] = self.info_TDAA.einlessen_datei()
+            self.import_attribute[3] = self.info_TDAA.einlesen_datei("Absicherungsaufträge")
 #           Else
 #               'Sammlung aller gesuchten Attribute erzeugen
 #               strAttributeTDAA = ""
@@ -455,7 +456,7 @@ class ATEStatus():
 #           'Dateiauswahl und Zuordnung
 #           If EinlesenDatei("Testfälle", strTFAttribute, rngTFAttribute, wbTF, wksTF, strFehlerTF, strDateinamen(4)) Then
 #               blnImportAttribute(4) = True
-            self.import_attribute[3] = self.info_TF.einlessen_datei()
+            self.import_attribute[3] = self.info_TF.einlesen_datei("Testfälle")
 #           Else
             if not self.import_attribute[3]:
 #               'Sammlung aller gesuchten Attribute erzeugen
@@ -499,7 +500,7 @@ class ATEStatus():
 #           'Dateiauswahl und Zuordnung
 #           If EinlesenDatei("FRU-Timing", strFRUTimingAttribute, rngFRUTimingAttribute, wbFRUTiming, wksFRUTiming, strFehlerFRUTiming, strDateinamen(5)) Then
 #               blnImportAttribute(5) = True
-            self.import_attribute[4] = self.info_fru_timming.einlessen_datei()
+            self.import_attribute[4] = self.info_fru_timming.einlesen_datei("FRU-Timing")
 #           Else
             if not self.import_attribute[4]:
 #               'Sammlung aller gesuchten Attribute erzeugen
@@ -544,7 +545,7 @@ class ATEStatus():
 #               'Dateiauswahl und Zuordnung
 #               If EinlesenDatei("Anforderungen Masterbereich", strAVWMasterAttribute, rngAVWMasterAttribute, wbAVWMaster, wksAVWMaster, strFehlerAVWMaster, strDateinamen(6)) Then
 #                   blnImportAttribute(6) = True
-                self.import_attribute[5] = self.info_AVW_master.einlessen_datei()
+                self.import_attribute[5] = self.info_AVW_master.einlesen_datei("Anforderungen Masterbereich")
 #               Else
                 if not self.import_attribute[5]:
 #                   'Sammlung aller gesuchten Attribute erzeugen
@@ -616,155 +617,7 @@ class ATEStatus():
 #
 # Funtion EinlesenDatei moved to om/db_info.py
 #
-#Public Function EinlesenDatei_Projektspezifisch(ByVal strTitel As String, ByRef strAttribute() As String, ByRef rngAttribute() As Range, ByRef wbImport As Workbook, ByRef wksImport As Worksheet, ByRef strFehler As String, ByRef strDateinamen As String, _
-#                                                ByVal strProjekt As String, ByRef strAttributeProjekt() As String, ByRef rngAttributeProjekt() As Range) As Boolean
-#Dim blnAttributeImport As Boolean   'Flag für Attribute
-#Dim blnAttributeProjektImport As Boolean    'Flag für projektspezifische Attribute
-#Dim intWksImport As Integer         'Zähler für Worksheets
-#Dim strImportPfad As String         'String für Dateipfad
-#Dim strImportDatei As String        'String für Dateinamen
-#Dim strFehlerGesamt As String       'Sting für Gesamtfehler
-#Dim intAttributeZaehler As Integer  'Zähler für Attribute
-#Dim strFehlerAttribute As String    'String für fehlende Attribute
-#Dim intAttributeProjektZaehler As Integer   'Zähler für Projekt-Attribute
-#Dim strFehlerProjektAttribute As String 'String für fehlende Projekt-Attribute
-#Dim blnErsterFund As Boolean        'Flag für erstes Auffinden des vollständigen Attributesatzes
-#Dim rngAttributeErsterFund() As Range   'Range-Array für erstes Auffinden des vollständigen allgemeinen Attributesatzes
-#Dim rngAttributeProjektErsterFund() As Range    'Range-Array für erstes Auffinden des vollständigen projektspezifischen Attributesatzes
-#Dim wksImportErsterFund As Worksheet    'Worksheet für erstes Auffinden des vollständigen Attributesatzes
-#Dim lngZeilenZahlerErsterFund As Long   'Long für letzte Zeile des ersten Fundes
-#Dim lngWeitererFundZaehler As Long  'Long für Startzeile des zusätzlichen Datensatzen
-#
-#On Error Resume Next
-#
-#blnAttributeImport = False
-#blnAttributeProjektImport = False
-#strFehlerGesamt = ""
-#strDateinamen = ""
-#blnErsterFund = False
-#
-#strImportPfad = Application.GetOpenFilename(FileFilter:="Excel-Dateien (*.xls; *.xlsx; *.xlm; *.xlsm), *.xls; *.xlsx; *.xlm; *.xlsm", FilterIndex:=1, Title:=strTitel & " auswählen")
-#If Trim(strImportPfad) <> "Falsch" Then
-#    'Datei öffnen
-#    Workbooks.Open Filename:=strImportPfad, ReadOnly:=True
-#    'Dateinamen extrahieren
-#    strImportDatei = Right(strImportPfad, Len(strImportPfad) - InStrRev(strImportPfad, "\"))
-#    strDateinamen = strImportDatei
-#    'Workbook zuweisen
-#    Set wbImport = Workbooks(strImportDatei)
-#    'Worksheets nach Attributen durchsuchen
-#    intWksImport = 0
-#    Do
-#        'Rangeobjekte zurücksetzen
-#        ReDim rngAttribute(LBound(strAttribute, 1) To UBound(strAttribute, 1))
-#        ReDim rngAttributeProjekt(LBound(strAttributeProjekt, 1) To UBound(strAttributeProjekt, 1))
-#        
-#        'Zähler für Arbeitsblatt erhöhen
-#        intWksImport = intWksImport + 1
-#        'Worksheet zuweisen
-#        Set wksImport = wbImport.Sheets(intWksImport)
-#        
-#        'Allgemeine Attribute suchen
-#        For intAttributeZaehler = LBound(strAttribute, 1) To UBound(strAttribute, 1)
-#            Set rngAttribute(intAttributeZaehler) = wksImport.Cells.Find(strAttribute(intAttributeZaehler), lookat:=xlWhole)
-#        Next intAttributeZaehler
-#        
-#        'Projekt-Attribute suchen
-#        For intAttributeProjektZaehler = LBound(strAttributeProjekt, 1) To UBound(strAttributeProjekt, 1)
-#            Set rngAttributeProjekt(intAttributeProjektZaehler) = wksImport.Cells.Find(strAttributeProjekt(intAttributeProjektZaehler), lookat:=xlWhole)
-#        Next intAttributeProjektZaehler
-#        
-#        'Suche der allgemeinen Attribute auswerten
-#        strFehlerAttribute = ""
-#        If RangeObjekteVorhandenFehlerausgabe(rngAttribute, strAttribute, strFehlerAttribute) Then
-#            blnAttributeImport = True
-#        Else
-#            If strFehlerGesamt = "" Then
-#                strFehlerGesamt = wksImport.Name & ": " & strFehlerAttribute
-#            Else
-#                strFehlerGesamt = strFehlerGesamt & "; " & wksImport.Name & ": " & strFehlerAttribute
-#            End If
-#        End If
-#        
-#        'Suche der Projekt-Attribute auswerten
-#        strFehlerProjektAttribute = ""
-#        If RangeObjekteVorhandenFehlerausgabe(rngAttributeProjekt, strAttributeProjekt, strFehlerProjektAttribute) Then
-#            blnAttributeProjektImport = True
-#        Else
-#            If strFehlerGesamt = "" Then
-#                strFehlerGesamt = wksImport.Name & ": " & strProjekt & " - " & strFehlerProjektAttribute
-#            Else
-#                strFehlerGesamt = strFehlerGesamt & "; " & wksImport.Name & ": " & strProjekt & " - " & strFehlerProjektAttribute
-#            End If
-#        End If
-#        
-#        'Auswertung beider Suchen (allgemein und projektspezifisch) und Unterscheidung, ob es das erste Auffinden des vollständigen Attributsatzes ist oder weitere
-#        'Beim ersten Auffinden werden die beiden Range-Arays gespeichert
-#        'Ab dem zweitem Auffinden des vollständigen Attributsatzes erfolgt eine Abfrage, eine Kopiervorganges der dazugehörigen Daten zum ersten Datensatz erfolgen soll und ggf. Umsetzung
-#        If blnAttributeImport = True And blnAttributeProjektImport = True Then
-#            'Zurücksetzen der Flags für den einzelnen Durchlauf
-#            blnAttributeImport = False
-#            blnAttributeProjektImport = False
-#            
-#            If blnErsterFund = False Then
-#                blnErsterFund = True
-#                'Felder dimensionieren
-#                ReDim rngAttributeErsterFund(LBound(strAttribute, 1) To UBound(strAttribute, 1))
-#                ReDim rngAttributeProjektErsterFund(LBound(strAttributeProjekt, 1) To UBound(strAttributeProjekt, 1))
-#                'Übernahme allgemeine Attribute
-#                For intAttributeZaehler = LBound(strAttribute, 1) To UBound(strAttribute, 1)
-#                    Set rngAttributeErsterFund(intAttributeZaehler) = rngAttribute(intAttributeZaehler)
-#                Next intAttributeZaehler
-#                'Übernahme projektspezifische Attribute
-#                For intAttributeProjektZaehler = LBound(strAttributeProjekt, 1) To UBound(strAttributeProjekt, 1)
-#                    Set rngAttributeProjektErsterFund(intAttributeProjektZaehler) = rngAttributeProjekt(intAttributeProjektZaehler)
-#                Next intAttributeProjektZaehler
-#                'Übernahme Arbeitsblatt
-#                Set wksImportErsterFund = wksImport
-#            Else
-#                If MsgBox("Weiterer Datensatz gefunden!" & vbCrLf & "Erster Fund: " & wksImportErsterFund.Name & vbCrLf & "Aktueller Fund: " & wksImport.Name & vbCrLf & vbCrLf & "Sollen die Datensätze zusammengeführt werden?", vbYesNo) = vbYes Then
-#                    'Alle Zeilen mit Einträgen durchlaufen und kopieren
-#                    For lngWeitererFundZaehler = 1 To wksImport.UsedRange.Rows.Count - rngAttribute(1).Row
-#                        If rngAttribute(1).Offset(lngWeitererFundZaehler, 0).Value <> "" Then
-#                            'Zielzeile ermitteln
-#                            lngZeilenZahlerErsterFund = rngAttributeErsterFund(1).End(xlDown).Row + 1
-#                            
-#                            'Allgemeine Werte kopieren
-#                            For intAttributeZaehler = LBound(strAttribute, 1) To UBound(strAttribute, 1)
-#                                wksImportErsterFund.Cells(lngZeilenZahlerErsterFund, rngAttributeErsterFund(intAttributeZaehler).Column).Value = rngAttribute(intAttributeZaehler).Offset(lngWeitererFundZaehler).Value
-#                            Next intAttributeZaehler
-#                            
-#                            'Projektspezifische Werte kopieren
-#                            For intAttributeProjektZaehler = LBound(strAttributeProjekt, 1) To UBound(strAttributeProjekt, 1)
-#                                wksImportErsterFund.Cells(lngZeilenZahlerErsterFund, rngAttributeProjektErsterFund(intAttributeProjektZaehler).Column).Value = rngAttributeProjekt(intAttributeProjektZaehler).Offset(lngWeitererFundZaehler).Value
-#                            Next intAttributeProjektZaehler
-#                        End If
-#                    Next lngWeitererFundZaehler
-#                End If
-#            End If
-#        End If
-#        
-#    'Loop While intWksImport < wbImport.Worksheets.Count And blnAttributeImport = False And blnAttributeProjektImport = False
-#    Loop While intWksImport < wbImport.Worksheets.Count
-#End If
-#
-#
-#'Rückgabe allgemeine Attribute
-#For intAttributeZaehler = LBound(strAttribute, 1) To UBound(strAttribute, 1)
-#    Set rngAttribute(intAttributeZaehler) = rngAttributeErsterFund(intAttributeZaehler)
-#Next intAttributeZaehler
-#'Rückgabe projektspezifische Attribute
-#For intAttributeProjektZaehler = LBound(strAttributeProjekt, 1) To UBound(strAttributeProjekt, 1)
-#    Set rngAttributeProjekt(intAttributeProjektZaehler) = rngAttributeProjektErsterFund(intAttributeProjektZaehler)
-#Next intAttributeProjektZaehler
-#'Rückgabe Arbeitsblatt
-#Set wksImport = wksImportErsterFund
-#
-#'Rückgabewert übernehmen
-#EinlesenDatei_Projektspezifisch = blnErsterFund
-#'Fehlerwert übernehmen
-#strFehler = strFehlerGesamt
-#End Function
+# Function EinlesenDatei_Projecktspezifish moved to om/db_info.py
 #
 #Private Sub SchliessenWb(ByVal wbBsM As Workbook, ByVal wbAVW As Workbook, ByVal wbAVWMaster As Workbook, ByVal wbTDVK As Workbook, ByVal wbTDAA As Workbook, ByVal wbTF As Workbook, ByRef wbFRUTiming As Workbook)
 #'wbBsM.Close SaveChanges:=False
