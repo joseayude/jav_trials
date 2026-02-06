66a6f4517c5cac6339dd5bace42cd6c968d03038
Jose Ayude Vazquez <jose.ayude@alten.es>
2026-02-06 14:20:00 +0100

    commented ate.data module

--- src/xls_management/ate/data.py
+++ src/xls_management/ate/data.py
@@ -1,26 +1,51 @@
 
 AVW_ATTRIBUTE_DE: tuple[str] = (
     "ID",
+#       strAVWAttribute(1) = "ID"
     "Dokument-ID",
+#       strAVWAttribute(2) = "Dokument-ID"
     "Basis für Testdesign",
+#       strAVWAttribute(3) = "Basis für Testdesign"
     "Typ",
+#       strAVWAttribute(4) = "Typ"
     "Kategorie",
+#       strAVWAttribute(5) = "Kategorie"
     "Status",
+#       strAVWAttribute(6) = "Status"
     "Feature",
+#       strAVWAttribute(7) = "Feature"
     "Reifegrad",
+#       strAVWAttribute(8) = "Reifegrad"
     "Umsetzer",
+#       strAVWAttribute(9) = "Umsetzer"
     "ASIL",
+#       strAVWAttribute(10) = "ASIL"
     "BSM-SaFuSi Bewertung",
+#       strAVWAttribute(11) = "BSM-SaFuSi Bewertung"
     "BSM-ZZ Bewertung",
+#       strAVWAttribute(12) = "BSM-ZZ Bewertung"
     "BSM-ED Bewertung",
+#       strAVWAttribute(13) = "BSM-ED Bewertung"
     "BSM-FFF Bewertung",
+#       strAVWAttribute(14) = "BSM-FFF Bewertung"
     "BSM-O Bewertung",
+#       strAVWAttribute(15) = "BSM-O Bewertung"
     "BSM-Se Bewertung",
+#       strAVWAttribute(16) = "BSM-Se Bewertung"
     "MV",
+#       strAVWAttribute(17) = "MV"
     "Cluster Testing",
+#       strAVWAttribute(18) = "Cluster Testing"
     "Dokument",
+#       strAVWAttribute(19) = "Dokument"
     "Kommentar Redaktionskreis",
+#       strAVWAttribute(20) = "Kommentar Redaktionskreis"
     "temp1_Text",
+#       strAVWAttribute(21) = "temp1_Text"
     "Anforderungsverantwortliche",
+#       strAVWAttribute(22) = "Anforderungsverantwortliche"
     "Abgezweigt aus",
+#       If blnAVWVorgaengerIDsVerwenden Then
+#           strAVWAttribute(23) = "Abgezweigt aus"  'strAVWAttribute(22) = "Abgezweigt aus"
+#       End If
 )
