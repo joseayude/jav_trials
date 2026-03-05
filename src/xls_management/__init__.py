20e50c7c55de1731098e07947fe1ffda9529c3d1
joseayude <jose.ayude@external.epiroc.com>
2026-03-05 11:53:54 +0100

    adapt HOMEPATH to Windows

--- src/xls_management/__init__.py
+++ src/xls_management/__init__.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 import os
+import platform
 
 ROOTPATH = Path(__file__).parent
-HOMEPATH = Path(os.environ['HOME'])
+home_var:str
+if platform.system():
+    home_var='USERPROFILE'
+else:
+    home_var='HOME'
+HOMEPATH = Path(os.getenv(home_var))
